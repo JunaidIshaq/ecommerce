da3e7e1425f8cf30ef13ea97357bac8766444d92
junaid <junaid.ishaq@yuzee.com>
2025-10-18 18:18:20 +0500

    Fixed Prerender Error

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -1,6 +1,14 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import {RenderMode, ServerRoute} from '@angular/ssr';
+import {routesIDs} from './routes-ids';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'product/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return routesIDs.map(id => ({ id }));
+    },
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
