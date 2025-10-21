325d9780a4dbbacd4bdfdd2b7cdb72a280047fe5
junaid <junaid.ishaq@yuzee.com>
2025-10-21 18:47:47 +0500

    Added Annotation @EnableCaching

--- backend/product-service/src/main/java/com/shopfast/productservice/ProductServiceApplication.java
+++ backend/product-service/src/main/java/com/shopfast/productservice/ProductServiceApplication.java
@@ -2,9 +2,11 @@ package com.shopfast.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @EnableMongoAuditing
+@EnableCaching
 @SpringBootApplication
 public class ProductServiceApplication {
 
