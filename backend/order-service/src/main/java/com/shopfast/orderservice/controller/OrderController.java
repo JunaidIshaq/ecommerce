eec48f521ba8dfe39bec3eb79ea3c9745d7e8d70
junaid <junaid.ishaq@yuzee.com>
2025-10-26 03:19:45 +0500

    Minor Update

--- backend/order-service/src/main/java/com/shopfast/orderservice/controller/OrderController.java
+++ backend/order-service/src/main/java/com/shopfast/orderservice/controller/OrderController.java
@@ -1,11 +1,8 @@
 package com.shopfast.orderservice.controller;
 
 import com.shopfast.orderservice.dto.OrderRequestDto;
-import com.shopfast.orderservice.dto.OrderResponseDto;
-import com.shopfast.orderservice.dto.PagedResponse;
 import com.shopfast.orderservice.model.Order;
 import com.shopfast.orderservice.service.OrderService;
-import com.shopfast.orderservice.util.InventoryMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -17,7 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
