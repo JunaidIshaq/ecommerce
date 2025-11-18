faf2c130f676e42880e385dad73c4f38b34696ad
root <root@srv1064473.hstgr.cloud>
2025-11-18 18:34:05 +0000

    Added elasticvue-config file

---
+++ backend/product-service/elasticvue-config/config.js
@@ -0,0 +1,11 @@
+window.elasticvue = {
+  server: {
+    basepath: "/elastic/"
+  },
+  clusters: [
+    {
+      name: "ShopFast ES",
+      uri: "https://shopfast.live/elasticsearch/"
+    }
+  ]
+};
