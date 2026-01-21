1e6edd6bcdca085a7fbf991d313b5907ea67deae
junaid <junaid.ishaq@yuzee.com>
2026-01-21 20:13:07 +0500

    Minor Update

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -1,16 +1,16 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
-
-export const serverRoutes: ServerRoute[] = [
-  {
-    path: '',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'product/:id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: '**',
-    renderMode: RenderMode.Server
-  }
-];
+// import { RenderMode, ServerRoute } from '@angular/ssr';
+//
+// export const serverRoutes: ServerRoute[] = [
+//   {
+//     path: '',
+//     renderMode: RenderMode.Server
+//   },
+//   {
+//     path: 'product/:id',
+//     renderMode: RenderMode.Server
+//   },
+//   {
+//     path: '**',
+//     renderMode: RenderMode.Server
+//   }
+// ];
