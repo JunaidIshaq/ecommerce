fbe94b2c74182546904f7e12ac2cf5ea2997b476
junaid <junaid.ishaq@yuzee.com>
2026-01-21 20:06:18 +0500

    Minor Update

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -1,25 +1,14 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
-import { routesIDs } from './routes-ids';
 
 export const serverRoutes: ServerRoute[] = [
-
-  // ✅ STATIC ROUTE (MANDATORY)
   {
     path: '',
-    renderMode: RenderMode.Prerender,
+    renderMode: RenderMode.Server
   },
-
-  // ✅ DYNAMIC ROUTE (OPTIONAL PRERENDER)
   {
     path: 'product/:id',
-    renderMode: RenderMode.Prerender,
-    async getPrerenderParams() {
-      console.log('VPS prerender routesIDs:', routesIDs);
-      return routesIDs.map(id => ({ id }));
-    },
+    renderMode: RenderMode.Server
   },
-
-  // ✅ WILDCARD MUST BE SSR (NOT PRERENDER)
   {
     path: '**',
     renderMode: RenderMode.Server
