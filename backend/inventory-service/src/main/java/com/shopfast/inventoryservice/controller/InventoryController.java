90c9de922ba182b03adfe03852d52898cfa3f525
junaid <junaid.ishaq@yuzee.com>
2026-03-14 17:33:24 +0500

    Added Request Header

--- backend/inventory-service/src/main/java/com/shopfast/inventoryservice/controller/InventoryController.java
+++ backend/inventory-service/src/main/java/com/shopfast/inventoryservice/controller/InventoryController.java
@@ -9,12 +9,14 @@ import com.shopfast.inventoryservice.util.InventoryMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.ws.rs.HeaderParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,7 +52,7 @@ public class InventoryController {
     @Operation(summary = "Get all inventory records")
     @GetMapping("/internal/admin/inventory/pageNumber/{pageNumber}/pageSize/{pageSize}")
     public ResponseEntity<PagedResponse<InventoryResponseDto>> getAllAdminInventoryItems(
-            @RequestParam(name = "userId") int userId,
+            @RequestHeader(name = "userId") String userId,
             @PathVariable(name = "pageNumber") int pageNumber,
             @PathVariable(name = "pageSize") int pageSize) {
         return ResponseEntity.ok(inventoryService.getAllInventoryItems(pageNumber, pageSize));
